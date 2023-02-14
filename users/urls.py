a94d45591e061809e0a45680cfb77ae6df0184b1
Teezeal <taiwotob@gmail.com>
2023-02-15 00:57:51 +0100

    'initial'

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import api_register_view
 
 urlpatterns = [
-    path("register", api_register_view, name="register")
+    path("register", api_register_view, name="register"),
+    path("login", obtain_auth_token, name="login")
 ]
